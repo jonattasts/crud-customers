0224c7fdd181a47aa0e6cc3e6a181a8e2d0d5dc2
jonatsts <jonatassantos96@hotmail.com>
2019-09-19 03:05:04 -0300

    adicionado crud de clientes

---
+++ crud_clientes/php_action/create.php
@@ -0,0 +1,22 @@
+<?php
+// Conexão
+
+require_once 'db_connect.php';
+
+if(isset($_POST['btn-cadastrar'])):
+	$nome = mysqli_escape_string($connect, $_POST['nome']);
+	$sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
+	$idade = mysqli_escape_string($connect, $_POST['idade']);
+	$email = mysqli_escape_string($connect, $_POST['email']);
+
+	$sql = "INSERT INTO cliente(nome, sobrenome, idade, email) VALUES ($nome, $sobrenome, $idade, $email)";
+
+	if(mysqli_query($connect, $sql)):
+		header('Location: index.php?sucesso');
+	else:
+		header('Location: index.php?erro');
+	endif;
+endif;
+
+
+?>
